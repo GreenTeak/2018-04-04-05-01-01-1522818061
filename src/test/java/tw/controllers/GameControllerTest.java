e619f0d4e8ae03db55c59119177eeb076b03171f
GreenTeak <GreenTeak1992@gmail.com>
2018-05-16 22:12:17 +0800

    testGameControllerTest

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,48 +1,84 @@
 package tw.controllers;
 
 import org.junit.Before;
-import org.junit.Rule;
+
 import org.junit.Test;
+
+import tw.commands.InputCommand;
 import tw.core.Answer;
 import tw.core.Game;
+import tw.core.exception.OutOfRangeAnswerException;
 import tw.core.generator.AnswerGenerator;
-import tw.core.generator.RandomIntGenerator;
+
 import tw.views.GameView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
+import static tw.core.GameStatus.SUCCESS;
 
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
-    GameController gameController;
-    ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
-    @Before
-    public void Setup()throws Exception{
-        Answer input=Answer.createAnswer("1 2 3 4");
-        RandomIntGenerator randomIntGenerator=new RandomIntGenerator();
-        AnswerGenerator answerGenerator=mock(AnswerGenerator.class);
-        when(answerGenerator.generate()).thenReturn(input);
-        Game game=new Game(answerGenerator);
-        GameView gameView=new GameView();
-        gameController=new GameController(game,gameView);
-    }
-  /*@Test
-  public void testshoebegin_should_be_return_printInfo() throws IOException{
-      System.setOut(new PrintStream(outputStream));
-      gameController.beginGame();
-      String result="------Guess Number Game, You have 6 chances to guess!  ------\n";
-     // System.out.print(","+result+",");
-      assertEquals(outputStream.toString(),result);
-  }*/
+    private Game game;
+    private Game gameSpy;
 
+    private GameController gameController;
+    private InputCommand command;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+
+    @Before
+    public void setup() throws OutOfRangeAnswerException {
+        System.setOut(new PrintStream(outContent));
+
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        game = new Game(answerGenerator);
+
+        gameSpy = spy(game);
+        command = mock(InputCommand.class);
+
+    }
+
+    @Test
+    public void should_print_game_success_or_fail_information() throws IOException {
+
+        when(gameSpy.checkCoutinue()).thenReturn(false);
+        when(gameSpy.checkStatus()).thenReturn(SUCCESS);
+        when(command.input()).thenReturn(Answer.createAnswer("1 2 3 4"));
+
+        gameController = new GameController(gameSpy,new GameView());
+        gameController.play(command);
+
+        String result = "Game Status: success\r\n";
+        String expected=outContent.toString();
+        assertThat(expected,equalTo(result));
+        //assertEquals(systemOut(),result);
+
+    }
+
+    @Test
+    public void should_print_game_information() throws IOException {
+        when(gameSpy.checkCoutinue()).thenReturn(true).thenReturn(false);
+
+        when(command.input()).thenReturn(Answer.createAnswer("2 4 7 8"));
+
+        gameController = new GameController(gameSpy,new GameView());
+        gameController.play(command);
+
+        String result ="Guess Result: 0A2B\r\n"+"Guess History:\r\n"+
+                "[Guess Numbers: 2 4 7 8, Guess Result: 0A2B]\r\n"+
+                "Game Status: continue\r\n" ;
+        String expected=outContent.toString();
+        assertThat(expected,equalTo(result));
+    }
 }
